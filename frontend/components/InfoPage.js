fbf7c08affc8135fa48fa35a5e9c9ae0d787ce2d
Mohitsingh Thakur <121103486+Moh1tsingh@users.noreply.github.com>
2023-09-10 17:36:11 +0530

    Add files via upload

---
+++ frontend/components/InfoPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InfoPage = () => {
+  return (
+    <div className="flex  justify-between">
+      <div >Information about our site.</div>
+      <div >Information about our site.</div>
+      <div >Information about our site.</div>
+    </div>
+  );
+};
+
+export default InfoPage;
