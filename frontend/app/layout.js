e9d00bd80a35a209512641a437ca60e026c0f0fa
Mohitsingh Thakur <121103486+Moh1tsingh@users.noreply.github.com>
2023-09-10 17:35:38 +0530

    Add files via upload

---
+++ frontend/app/layout.js
@@ -0,0 +1,12 @@
+import "./globals.css";
+import Nav from '../components/Nav'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Nav/>
+        {children}</body>
+    </html>
+  );
+}
